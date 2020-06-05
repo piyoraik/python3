2154ff0ae855449fba37c6a1eba46a2f3b98da89
raityupiyo <42093052+piyoraik@users.noreply.github.com>
2020-06-06 00:28:34 +0900

    日本語入力、日本語表示に対応

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,6 @@
 FROM python:3.8-alpine
+ENV LANG=C.UTF-8
+ENV LANGUAGE=en_US:
 RUN mkdir /app
 RUN pip install pymysql
 WORKDIR /app
--- app/test.py
+++ app/test.py
@@ -1,12 +1,14 @@
 import pymysql
 
-sql = "CREATE TABLE test (id int, name varchar(10))"
+# sql = "CREATE TABLE test (id int, name varchar(10))"
+sql = "INSERT INTO test VALUES(10,'あ')"
 
 connect = pymysql.connect(host='mysql',
                          port=3306,
                          user='root',
                          password='example',
-                         db='test')
+                         db='test',
+                         charset='utf8mb4')
 cursor = connect.cursor()
 cursor.execute(sql)
 connect.commit()
